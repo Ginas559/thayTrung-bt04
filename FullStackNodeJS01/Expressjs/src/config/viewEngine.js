5d601e7b3d132d1e1bce57c98742575ef25d3a6b
Ginas559 <levanphong511@gmail.com>
2026-05-17 22:33:09 +0700

    Fix: absolute path routing for ejs views using __dirname

--- FullStackNodeJS01/Expressjs/src/config/viewEngine.js
+++ FullStackNodeJS01/Expressjs/src/config/viewEngine.js
@@ -2,12 +2,13 @@ const path = require('path');
 const express = require('express');
 
 const configViewEngine = (app) => {
-    // Cấu hình thư mục views và template engine là ejs
-    app.set('views', path.join('./src', 'views'));
+    // Định vị chính xác thư mục views bằng đường dẫn tuyệt đối __dirname
+    // Vì file này nằm trong src/config, cần đi ra 1 cấp (..) rồi đi vào views
+    app.set('views', path.join(__dirname, '..', 'views'));
     app.set('view engine', 'ejs');
 
-    // Cấu hình các file tĩnh (static files): image/css/js
-    app.use(express.static(path.join('./src', 'public')));
+    // Cấu hình các file tĩnh bằng đường dẫn tuyệt đối (đi ra 1 cấp rồi vào public)
+    app.use(express.static(path.join(__dirname, '..', 'public')));
 }
 
 module.exports = configViewEngine;
